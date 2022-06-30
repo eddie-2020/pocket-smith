9c3b531e0d1eaca9a7ec01b17dc419893b3991c7
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-30 16:35:33 -0700

    Pocket smith - add models

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_30_233109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_groups_on_author_id"
+  end
+
+  create_table "groups_operations", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "operation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_groups_operations_on_group_id"
+    t.index ["operation_id"], name: "index_groups_operations_on_operation_id"
+  end
+
+  create_table "operations", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "amount", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_operations_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "operations", "users", column: "author_id"
 end

745cca23506d3d933b6f8e0b03a2523c9386a58a
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-01 04:42:46 -0700

    Pocket smith - add integration specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     name { 'Pedro Guerreiro' }
-    email { 'pedro@mail.com' }
-    password { '123456' }
-    password_confirmation { '123456' }
+    email { 'oluyaratosin123@mail.com' }
+    password { 'root123' }
+    password_confirmation { 'root123' }
 
     trait :confirmed do
       confirmed_at { Time.zone.now }

5d24304a701f159c3ec20113f1a7066c2b7c21fe
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-07-01 03:05:30 -0700

    Pocket smith - add complete styling to pocket smith app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :groups, only: %i[index new create] do
     resources :operations, only: %i[index]
   end

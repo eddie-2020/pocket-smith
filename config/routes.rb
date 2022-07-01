e78c2c1360e6f31c55378eb100eea365ddb22e99
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-30 18:23:31 -0700

    Pocket smith - add controllers for all model features

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  resources :groups, only: %i[index, new, create] do
+    resources :operations, only: %i[index]
+  end
+
+  resources :operations, only: %i[new, create]
 end
